292132018d5ab0960ac3e66159db6ae45bae65fa
rlaalswhd <rlaalswhd200@naver.com>
2023-07-12 13:38:49 +0900

    Initial Commit

---
+++ EarTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EarTraining
+//
+//  Created by 김민종 on 2023/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
